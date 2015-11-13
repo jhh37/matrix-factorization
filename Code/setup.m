f8ed69ba8e34ce61ab4ee75d532709b1e465e706
Je Hyeong Hong <jhh37@cam.ac.uk>
2015-11-13 08:28:07 +0000

    Setup code fix

--- Code/setup.m
+++ Code/setup.m
@@ -4,16 +4,16 @@ try
   au_sparse(int32([1 2 3 4]), int32([1 3 5 7]), randn(4,1));
 catch
   disp('Setting up AWFUL...');
-  cd External/awful/matlab
+  cd Toolboxes/awful/matlab
   addpath(genpath(pwd));
-  au_mexall
+  mex -largeArrayDims au_sparse.cxx
   cd ../../..
 end
 
 % Manopt
 if exist('manopt_version', 'file') ~= 2
   disp('Setting up Manopt...');
-  cd 'External/manopt';
+  cd 'Toolboxes/manopt';
   importmanopt;
   cd ../..
 end
@@ -26,11 +26,11 @@ cd ..
 
 cd Algorithms
 addpath(genpath(pwd));
-cd JE
-rmpath([pwd, '/ce_src/build']);
-rmpath([pwd, '/ce_src/LRMC']);
-rmpath([pwd, '/ce_src']);
-cd ..
+% cd JE
+% rmpath([pwd, '/ceres_lrmc/build']);
+% rmpath([pwd, '/ce_src/LRMC']);
+% rmpath([pwd, '/ce_src']);
+% cd ..
 
 %% COMPILE MEX FILES
 % JE
